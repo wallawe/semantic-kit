9b38e1253900a03ad818b3a84882b5e66504ab8c
Travis Valentine <tvalent2@gmualumni.org>
2014-05-26 14:22:28 -0400

    Duh, the token param was wrong. Now recipient gets the card assigned

--- app/controllers/payment_preferences_controller.rb
+++ app/controllers/payment_preferences_controller.rb
@@ -4,7 +4,7 @@ class PaymentPreferencesController < ApplicationController
 
   def create
     # if card
-    current_user.stripe_account.update_from_card(params[:token])
+    current_user.stripe_account.update_from_card(params[:stripeToken])
     redirect_to themes_path
     # if bank_account
     # current_user.stripe_account.update_from_bank_account(token)
