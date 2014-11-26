010c16df708f7ad8d0b696c85accb69a8c2627a8
Travis Valentine <tvalent2@gmualumni.org>
2014-11-26 11:44:51 -0500

    Add admin UI for theme approval, add image field to snippet for uploading/editing

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,8 +1,13 @@
 class Admin::DashboardsController < ApplicationController
 
   def show
+<<<<<<< HEAD
     redirect_to semantic_ui_themes_path unless current_user.admin?
+=======
+    redirect_to themes_path unless current_user && current_user.admin?
+>>>>>>> Add admin tables for themes, image to snippets
 
     @themes = Theme.pending
+    @snippets = Snippet.all
   end
 end
