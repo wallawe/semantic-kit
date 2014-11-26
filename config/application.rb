94b0d7250a370d9812df384ba54bf9276c1a26e4
Will Wallace <william.wallace@livingsocial.com>
2014-11-26 09:45:26 -0500

    Move config stuff to production.rb to see if that fixes the broken images in prod

--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,5 @@ module WrapSemantic
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile << Proc.new { |path|
-      if path =~ /\.(eot|svg|ttf|woff)\z/
-        true
-      end
-    }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ WrapSemantic::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.assets.precompile += %w(.svg .eot .woff .ttf)
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
