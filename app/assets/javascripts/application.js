12590551ece5c47a6d29902706e4d52910e0d9a3
Will Wallace <william.wallace@livingsocial.com>
2014-11-28 01:15:09 -0500

    Fix favorite when not signed in and fix accordian on faq

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,14 @@ $(document).on('click', '.dropdown', function() {
   $(this).dropdown('show');
 }).on('mouseleave', '.dropdown', function(){
   $('.ui.dropdown').dropdown('hide');
-})
-
+});
 
 window.onload = function(e) {
   $('.tooltip').popup({
     position: 'top center'
   });
+
+  $('.ui.accordion').accordion();
 
   $('#tabs a').on('click', function(){
     $('#tabs a').removeClass('active');
@@ -34,5 +35,5 @@ window.onload = function(e) {
     var current = $(this).attr('href');
     $(current).show();
     return false;
-  })
+  });
 };
