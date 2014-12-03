3086d09a1f5919d572ee8a1c189b0e94581df0a6
Will Wallace <william.wallace@livingsocial.com>
2014-12-03 00:07:53 -0500

    Remove changes in subscriptions to see if that fixes the issue

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,27 @@
 class SubscriptionsController < ApplicationController
+  # def create
+  #   theme = Theme.find_by_id(params[:id])
+
+  #   # Stripe expects amount to be in cents
+  #   amount = params[:price].to_i * 100
+
+  #   subscription = current_user.subscriptions.create!(theme_id: params[:id], price_tier: params[:price])
+
+  #   if params[:paypal]
+  #     theme.sales_tracker.increment!(params[:count].to_sym)
+  #   else
+
+  #     subscription.create_customer_and_charge(params[:token], amount)
+
+  #     theme.pay_owner(amount)
+  #   rescue Stripe::CardError => e
+  #     flash[:error] = e.message
+  #     redirect_to theme_path(theme)
+  #   end
+
+  #   redirect_to theme_path(theme), notice: "Thanks for purchasing #{theme.name}"
+  # end
+
   def create
     theme = Theme.find_by_id(params[:id])
 
@@ -7,19 +30,16 @@ class SubscriptionsController < ApplicationController
 
     subscription = current_user.subscriptions.create!(theme_id: params[:id], price_tier: params[:price])
 
-    if params[:paypal]
-      theme.sales_tracker.increment!(params[:count].to_sym)
-    else
+    subscription.create_customer_and_charge(params[:token], amount)
 
-      subscription.create_customer_and_charge(params[:token], amount)
+    theme.pay_owner(amount)
 
-      theme.pay_owner(amount)
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to theme_path(theme)
-    end
+    redirect_to theme_path(theme), notice: "WOOHOO"
 
-    redirect_to theme_path(theme), notice: "Thanks for purchasing #{theme.name}"
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to theme_path(theme)
   end
+
 
 end
