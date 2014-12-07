7eee2bdb509654364061e6c6f4df7e1e846029b0
Will Wallace <william.wallace@livingsocial.com>
2014-12-07 17:01:21 -0500

    Change upload size of images from 375x234 to 600x375

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [375, 600]
+  process :resize_to_fit => [600, 375]
   #
   # def scale(width, height)
   #   # do something
