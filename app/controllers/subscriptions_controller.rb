e476883195eba2f4a054da8fb20cb6564f51714f
Will Wallace <william.wallace@livingsocial.com>
2014-12-07 16:16:00 -0500

    Copy change and minor fix in subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,11 +6,9 @@ class SubscriptionsController < ApplicationController
 
     subscription = current_user.subscriptions.create!(theme_id: params[:id], price_tier: params[:price])
 
-    if params[:paypal]
-      theme.sales_tracker.increment!(params[:count].to_sym)
-    else
+    theme.sales_tracker.increment!(params[:count].to_sym) if params[:paypal]
 
     redirect_to theme_path(theme), notice: "Thanks for purchasing #{theme.name}"
-  end
 
+  end
 end
