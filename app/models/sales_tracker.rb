7adc509545e799e3595ac5121cd73f6086aed6e5
Travis Valentine <tvalent2@gmualumni.org>
2014-12-09 20:00:52 -0500

    Actually calculate revenue maybe?

--- app/models/sales_tracker.rb
+++ app/models/sales_tracker.rb
@@ -2,9 +2,8 @@ class SalesTracker < ActiveRecord::Base
   belongs_to :theme
 
   def revenue
-    0
-    # single_tier_count * theme.price_list.single_tier +
-    # multiple_tier_count * theme.price_list.multiple_tier +
-    # extended_tier_count * theme.price_list.extended_tier
+    (single_tier_count || 0) * theme.price_list.single_tier +
+    (multiple_tier_count || 0) * theme.price_list.multiple_tier +
+    (extended_tier_count || 0) * theme.price_list.extended_tier
   end
 end
