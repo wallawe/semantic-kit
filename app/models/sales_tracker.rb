05a5d2d4363202e56ab5976e56bcfba33f5add83
Travis Valentine <tvalent2@gmualumni.org>
2014-12-09 20:32:16 -0500

    Calculate revenue as 30 percent of gross

--- app/models/sales_tracker.rb
+++ app/models/sales_tracker.rb
@@ -2,6 +2,10 @@ class SalesTracker < ActiveRecord::Base
   belongs_to :theme
 
   def revenue
+    gross_sales * 0.3
+  end
+
+  def gross_sales
     (single_tier_count || 0) * theme.price_list.single_tier +
     (multiple_tier_count || 0) * theme.price_list.multiple_tier +
     (extended_tier_count || 0) * theme.price_list.extended_tier
