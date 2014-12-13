c3c0cc10a5e9e626d144ac4426df23adebcf257b
Travis Valentine <tvalent2@gmualumni.org>
2014-12-13 16:21:46 -0500

    Add test for guest email

--- spec/mailers/theme_mailer_spec.rb
+++ spec/mailers/theme_mailer_spec.rb
@@ -5,12 +5,27 @@ RSpec.describe ThemeMailer, :type => :mailer do
   let(:theme) { Theme.create(name: "Admin Theme", description: "stuff", preview_url: "url", file_package: "stuff.zip", image: "image.png") }
 
   describe "user_purchase" do
-    it "send user password reset url" do
+    it "sends a link to the theme" do
       mail = ThemeMailer.user_purchase(theme, user)
       mail.subject.should == "Thanks for purchasing Admin Theme"
       mail.to.should == [user.email]
       mail.from.should == ["help@semantickit.com"]
       mail.body.encoded.should include(theme_path(theme))
+    end
+  end
+
+  describe "#guest_purchase" do
+    it "sends a link to the theme" do
+      # @theme = theme
+      # @email = guest_email
+      # @token = token
+      # mail(:to => "#{@email} <#{@email}>", subject: t(:"mailers.themes.purchase_subject", theme_name: theme.name))
+
+      mail = ThemeMailer.guest_purchase(theme, "test@testing.com", "1214u10934098")
+      mail.subject.should == "Thanks for purchasing Admin Theme"
+      mail.to.should == ["test@testing.com"]
+      mail.from.should == ["help@semantickit.com"]
+      mail.body.encoded.should include(theme_path(theme, token: "1214u10934098"))
     end
   end
 end
