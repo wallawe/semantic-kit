33a140d6aec0ea358821e3b786797ce6bd3aa350
Travis Valentine <tvalent2@gmualumni.org>
2014-12-15 10:32:22 -0500

    Redirect to root path if no return_to is set

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
   def create
     if user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
       session[:user_id] = user.id
-      redirect_to session.delete(:return_to), :notice => t(:"sessions.successful_login")
+      path = session.delete(:return_to) || root_path
+      redirect_to path, :notice => t(:"sessions.successful_login")
     else
       flash.now[:alert] = t(:"sessions.invalid_login")
       render :action => 'new'
