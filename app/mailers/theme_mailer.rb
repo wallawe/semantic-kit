1ee1b0791bb99c67db3eb9833ce2dc1adb19ecc1
Travis Valentine <travis@yourturn.org>
2016-04-28 19:02:11 -0400

    Bcc us on purchase emails so we can look at the timing of purchase vs mail

--- app/mailers/theme_mailer.rb
+++ app/mailers/theme_mailer.rb
@@ -2,17 +2,19 @@ class ThemeMailer < ActionMailer::Base
   default :from => "SemanticKit <help@semantickit.com>",
           :return_path => "help@semantickit.com"
 
+  BCC_EMAILS = ["wallac.will@gmail.com", "tvalent2@gmail.com"]
+
   def user_purchase(theme, user)
     @theme = theme
     @user  = user
-    mail(:to => "#{@user.username} <#{@user.email}>", subject: t(:"mailers.themes.purchase_subject", theme_name: theme.name))
+    mail(:to => "#{@user.username} <#{@user.email}>", bcc: BCC_EMAILS, subject: t(:"mailers.themes.purchase_subject", theme_name: theme.name))
   end
 
   def guest_purchase(theme, guest_email, token)
     @theme = theme
     @email = guest_email
     @token = token
-    mail(:to => "#{@email} <#{@email}>", subject: t(:"mailers.themes.purchase_subject", theme_name: theme.name))
+    mail(:to => "#{@email} <#{@email}>", bcc: BCC_EMAILS, subject: t(:"mailers.themes.purchase_subject", theme_name: theme.name))
   end
 
 end
